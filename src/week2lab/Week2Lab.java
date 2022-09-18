33162299d7e1fd52d1e5e7240a5a657e68840daa
ilyes <ilyes@MSI>
2022-09-18 10:46:21 -0600

    Added code to print "Hello World!"

---
+++ src/week2lab/Week2Lab.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week2lab;
+
+/**
+ *
+ * @author ilyes
+ */
+public class Week2Lab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is my totally creative Java Application for week 2!");
+    }
+    
+}
